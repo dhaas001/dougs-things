3bbfe46cbcaa54bd36dae45782c5645a3bc6d7f7
doug.haas <doug.haas@ISS206931.issinc.com>
2020-03-23 14:15:24 -0600

    initial testcommon

---
+++ testcommon/src/main/java/entity/InventoryCategory.java
@@ -0,0 +1,70 @@
+package com.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+@Entity
+@Table(name="Inventory_Category")
+public class InventoryCategory
+{
+   private int version;
+   private int id;
+
+   private String categoryName;
+   private String categoryDescription;
+
+
+   public InventoryCategory(){}
+
+   @Column(name = "categoryName")
+   public String getCategoryName()
+   {
+      return categoryName;
+   }
+
+   public void setCategoryName(String name)
+   {
+      this.categoryName = name;
+   }
+
+   @Column(name = "itemDescription")
+   public String getCategoryDescription()
+   {
+      return categoryDescription;
+   }
+
+   public void setCategoryDescription(String description)
+   {
+      this.categoryDescription = description;
+   }
+
+   @Version
+   @Column(name = "version_field")
+   // not required
+   public int getVersion()
+   {
+       return version;
+   }
+
+   public void setVersion(int version)
+   {
+      this.version = version;
+   }
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   public int getId()
+   {
+      return id;
+   }
+
+   public void setId(int id)
+   {
+      this.id = id;
+   }
+}

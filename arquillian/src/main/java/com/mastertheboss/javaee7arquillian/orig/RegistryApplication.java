cf49c0f3c36bebed4dbbb0d620ba1df9b802457c
doug.haas <doug.haas@ISS206931.issinc.com>
2020-04-02 15:54:12 -0600

    remove unneeded classes

--- arquillian/src/main/java/com/mastertheboss/javaee7arquillian/orig/RegistryApplication.java
+++
@@ -1,9 +0,0 @@
-package com.mastertheboss.javaee7arquillian.orig;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("registry")
-public class RegistryApplication extends Application {
-
-}

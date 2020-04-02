5e5ac3eaba0bc187e5f3455163202dfad70b6492
doug.haas <doug.haas@ISS206931.issinc.com>
2020-04-02 14:39:47 -0600

    rename and repackage

---
+++ arquillian/src/main/java/com/arquillian/test/RegistryApplication.java
@@ -0,0 +1,9 @@
+package com.arquillian.test;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("registry")
+public class RegistryApplication extends Application {
+
+}
